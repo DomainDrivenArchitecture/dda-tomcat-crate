595f009bb1715dd06000a9d13897dfd0301652ab
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-05-21 12:24:00 +0200

    updeate deps

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [prismatic/schema "1.1.1"]
-                 [metosin/schema-tools "0.9.0"]
+                 [metosin/schema-tools "0.10.0-SNAPSHOT"]
                  [com.palletops/pallet "0.8.12"]
                  [com.palletops/stevedore "0.8.0-beta.7"]
-                 [org.domaindrivenarchitecture/dda-config-commons "0.1.2-SNAPSHOT"]
+                 [org.domaindrivenarchitecture/dda-config-commons "0.1.4"]
                  [org.domaindrivenarchitecture/dda-config-crate "0.3.4-SNAPSHOT"]]
   :profiles {:dev
              {:dependencies
