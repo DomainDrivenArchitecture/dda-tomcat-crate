2f6cfcd503c63c9e33f5475a3efe92c5cbeef323
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-09-01 14:53:53 +0200

    updated deps

--- project.clj
+++ project.clj
@@ -1,29 +1,27 @@
-(defproject org.domaindrivenarchitecture/dda-tomcat-crate "0.1.6-SNAPSHOT"
-  :description "The dda tomcat crate"
+(defproject dda/dda-tomcat-crate "0.2.0-SNAPSHOT"
+  :description "tomcat crate of dda-pallet project"
   :url "https://www.domaindrivenarchitecture.org"
-  :pallet {:source-paths ["src"]}
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [prismatic/schema "1.1.3"]
                  [com.palletops/pallet "0.8.12"]
                  [com.palletops/stevedore "0.8.0-beta.7"]
-                 [org.domaindrivenarchitecture/dda-config-commons "0.1.7"]
-                 [org.domaindrivenarchitecture/dda-pallet "0.3.0"]]
+                 [dda/dda-pallet "0.3.0"]]
   :profiles {:dev
-             {:dependencies
-              [[com.palletops/pallet "0.8.12" :classifier "tests"]
-               ]
+             {:source-paths ["integration"]
+              :resource-paths ["dev-resources"]
+              :dependencies
+              [[org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
+               [com.palletops/pallet "0.8.12" :classifier "tests"]
+               [dda/dda-serverspec-crate "0.2.2-SNAPSHOT"]
+               [ch.qos.logback/logback-classic "1.2.3"]
+               [org.slf4j/jcl-over-slf4j "1.7.25"]]
               :plugins
-              [[com.palletops/pallet-lein "0.8.0-alpha.1"]
-               [lein-sub "0.3.0"]]}
-              :leiningen/reply
+              [[lein-sub "0.3.0"]]}
+             :leiningen/reply
                {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.22"]]
                 :exclusions [commons-logging]}}
    :local-repo-classpath true
-   :repositories [["snapshots" :clojars]
-                  ["releases" :clojars]]
-   :deploy-repositories [["snapshots" :clojars]
-                         ["releases" :clojars]]
-   :classifiers {:tests {:source-paths ^:replace ["test"]
-                         :resource-paths ^:replace []}})
+   :classifiers {:tests {:source-paths ^:replace ["test" "integration"]
+                         :resource-paths ^:replace ["dev-resources"]}})
