b376f5c888db8c4f186a11b16fa856e55183e741
Jan Krebs <jan.krebs@meissa-gmbh.de>
2018-01-04 13:02:11 +0100

    add expectation of setenv generation from liferay

--- test/src/dda/pallet/dda_tomcat_crate/infra/app_config_test.clj
+++ test/src/dda/pallet/dda_tomcat_crate/infra/app_config_test.clj
@@ -26,3 +26,24 @@
     (is
       (= expected-setenv-sh-lines
          (sut/setenv-sh setenv-sh-config)))))
+
+;Aus Liferay:
+(def expected-liferay-setenv
+  ["TOMCAT7_USER=tomcat7"
+   "TOMCAT7_GROUP=tomcat7"
+   "JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-amd64"
+   (str "JAVA_OPTS=\"-Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true "
+        "-Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false "
+        "-Duser.timezone=GMT"
+        "-Xms1536m -Xmx2560m -XX:MaxPermSize=512m -XX:+UseConcMarkSweepGC\"")
+   "#JAVA_OPTS=\"${JAVA_OPTS} -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n\""
+   "CATALINA_OPTS=\"-Dcustom.lr.dir=/var/lib/liferay\""
+   "TOMCAT7_SECURITY=no"
+   "#AUTHBIND=no"]
+  )
+
+(deftest test-liferay-setenv
+  (testing
+    (is (= (generate-setenv liferay-setenv-config)
+           expected-liferay-setenv))))
+
