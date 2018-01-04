1f86d19cab5a5026903294be60d42d2a175222aa
jem <michael.jerger@meissa-gmbh.de>
2018-01-04 21:55:11 +0100

    merged liferay-config-expectation

--- test/src/dda/pallet/dda_tomcat_crate/infra/tomcat_vm_test.clj
+++ test/src/dda/pallet/dda_tomcat_crate/infra/tomcat_vm_test.clj
@@ -28,16 +28,42 @@
    :max-perm-size "3m"
    :custom {:config-setenv-sh-location ""}})
 
+(def liferay-setenv-sh-config
+  {:os-user "tomcat7"
+   :java-home "/usr/lib/jvm/java-1.6.0-openjdk-amd64"
+   :xms "1536m"
+   :xmx "2560m"
+   :max-perm-size "512m"
+   :settings #{:prefer-ipv4 :disable-cl-clear-ref :conc-mark-sweep-gc :disable-tomcat-security}
+   :custom {:config-setenv-sh-location ""}})
+
 (def expected-setenv-sh-lines
   ["JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64"
    (str "JAVA_OPTS=\"$JAVA_OPTS -server -Dfile.encoding=UTF8"
         " -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false"
         " -Duser.timezone=GMT -Xms1m -Xmx2m -XX:MaxPermSize=3m\"")])
 
+(def expected-liferay-setenv-sh-lines
+  ["TOMCAT7_USER=tomcat7"
+   "TOMCAT7_GROUP=tomcat7"
+   "JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-amd64"
+   (str "JAVA_OPTS=\"-Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true "
+        "-Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false "
+        "-Duser.timezone=GMT -Xms1536m -Xmx2560m -XX:MaxPermSize=512m "
+        "-XX:+UseConcMarkSweepGC\"")
+   "#JAVA_OPTS=\"${JAVA_OPTS} -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n\""
+   "CATALINA_OPTS=\"-Dcustom.lr.dir=/var/lib/liferay\""
+   "TOMCAT7_SECURITY=no"])
+
 (deftest test-setenv-sh
   (testing
     (is
       (s/validate sut/TomcatVmConfig setenv-sh-config))
     (is
+      (s/validate sut/TomcatVmConfig liferay-setenv-sh-config))
+    (is
       (= expected-setenv-sh-lines
-         (sut/setenv-sh setenv-sh-config)))))
+         (sut/setenv-sh setenv-sh-config)))
+    (is
+      (= expected-liferay-setenv-sh-lines
+         (sut/setenv-sh liferay-setenv-sh-config)))))
