f81f9aadf7cce2f4da045ec634af366374b9a5d8
jem <michael.jerger@meissa-gmbh.de>
2018-05-10 16:58:31 +0200

    Version 2.0.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject dda/dda-tomcat-crate "2.0.0-SNAPSHOT"
+(defproject dda/dda-tomcat-crate "2.0.0"
   :description "tomcat crate of dda-pallet project"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [dda/dda-pallet "2.1.0-SNAPSHOT"]
+                 [dda/dda-pallet "2.1.2"]
                  [selmer "1.11.7"]]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
@@ -21,14 +21,14 @@
               :dependencies
               [[org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
                [com.palletops/pallet "0.8.12" :classifier "tests"]
-               [dda/dda-serverspec-crate "1.0.4-SNAPSHOT"]
+               [dda/dda-serverspec-crate "1.0.6-SNAPSHOT"]
                [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-               [org.slf4j/jcl-over-slf4j "1.8.0-beta1"]]
+               [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
               :plugins
               [[lein-sub "0.3.0"]]}
              :repl-options {:init-ns dda.pallet.dda-tomcat-crate.app.instantiate-aws}
              :leiningen/reply
-             {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta1"]]
+             {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
               :exclusions [commons-logging]}
              :test {:test-paths ["test/src"]
                     :resource-paths ["test/resources"]
@@ -37,5 +37,5 @@
                        :resource-paths ["uberjar/resources"]
                        :aot :all
                        :main dda.pallet.dda-tomcat-crate.main
-                       :dependencies [[org.clojure/tools.cli "0.3.5"]]}}
+                       :dependencies [[org.clojure/tools.cli "0.3.7"]]}}
    :local-repo-classpath true)
