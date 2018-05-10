204a70eb3b8f5802abc9321a98df27d1fcd76396
jem <michael.jerger@meissa-gmbh.de>
2018-05-10 17:11:05 +0200

    use released serverspec

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
               :dependencies
               [[org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
                [com.palletops/pallet "0.8.12" :classifier "tests"]
-               [dda/dda-serverspec-crate "1.0.6-SNAPSHOT"]
+               [dda/dda-serverspec-crate "1.0.5"]
                [ch.qos.logback/logback-classic "1.3.0-alpha4"]
                [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
               :plugins
