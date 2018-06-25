9959bd569d9028ea44ed4821337fea07396be7ba
Jan Krebs <jan.krebs@meissa-gmbh.de>
2018-06-25 11:06:27 +0200

    Version 2.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-tomcat-crate "2.0.1-SNAPSHOT"
+(defproject dda/dda-tomcat-crate "2.0.1"
   :description "tomcat crate of dda-pallet project"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
